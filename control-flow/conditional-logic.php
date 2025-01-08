af35006a4211b561a060c56437bfb84448b209ff
gcpearse <glen.c.pearse@gmail.com>
2025-01-08 22:23:22 +0000

    Adds example of if...else with alternative syntax

---
+++ control-flow/conditional-logic.php
@@ -0,0 +1,23 @@
+<?php
+
+// Note the alternative syntax using colons and endif
+function display_grade($score): void
+{
+    if ($score >= 70):
+        echo "$score% - A";
+    elseif ($score >= 60):
+        echo "$score% - B";
+    elseif ($score >= 50):
+        echo "$score% - C";
+    elseif ($score >= 40):
+        echo "$score% - D";
+    else:
+        echo "$score% - F";
+    endif;
+
+    echo '<br>';
+}
+
+for ($i = 0; $i < 10; $i++) {
+    display_grade(rand(1, 100));
+}
