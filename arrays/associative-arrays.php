6a3159b124ea76a9374e2aee3e304da69caffc23
gcpearse <glen.c.pearse@gmail.com>
2025-01-09 18:28:39 +0000

    Adds example of associative arrays

---
+++ arrays/associative-arrays.php
@@ -0,0 +1,12 @@
+<?php
+
+$currencies = [
+    'LAK' => 'Lao Kip',
+    'THB' => 'Thai Baht',
+];
+
+$currencies['VND'] = 'Vietnamese Dong';
+
+print_r($currencies);
+
+echo $currencies['THB'] . "\n";
