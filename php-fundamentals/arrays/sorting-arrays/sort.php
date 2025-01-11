b1ba2aa0aaf05f27d28e6c7caff1c9c3cd86ff31
gcpearse <glen.c.pearse@gmail.com>
2025-01-11 19:41:53 +0000

    Moves examples of rsort() into file

--- php-fundamentals/arrays/sorting-arrays/sort.php
+++ php-fundamentals/arrays/sorting-arrays/sort.php
@@ -5,7 +5,13 @@ $scores = [90, 45, 78, 34, 67, 98, 33];
 sort($scores);
 print_r($scores);
 
+rsort($scores);
+print_r($scores);
+
 $flight_numbers = ['BA100', 'BA200', 'BA1234', 'BA2345', 'BA300'];
 
 sort($flight_numbers, SORT_STRING | SORT_NATURAL);
+print_r($flight_numbers);
+
+rsort($flight_numbers, SORT_STRING | SORT_NATURAL);
 print_r($flight_numbers);
