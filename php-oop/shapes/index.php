5cee44f1a07ed3b4bdf1a16dc886551e5a386aef
gcpearse <glen.c.pearse@gmail.com>
2025-01-15 13:02:29 +0000

    Instantiates a Rectangle object to demo functionality

--- php-oop/shapes/index.php
+++ php-oop/shapes/index.php
@@ -1,9 +1,17 @@
 <?php
 
 require 'circle.php';
+require 'rectangle.php';
 
 $circle = new Circle(10);
 echo $circle;
 
 $circle->setRadius(20);
 echo $circle;
+
+$square = new Rectangle(10, 10);
+echo $square;
+
+$square->setHeight(5);
+$square->setWidth(5);
+echo $square;
